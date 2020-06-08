f5ad93869c3c1de3f39263b64f186bf571ac4d36
jason.khadka <jasonkhadka2000@gmail.com>
2020-06-08 09:38:48 +0545

    trail commit

--- infix.py
+++ infix.py
@@ -2,7 +2,7 @@ def prefixfunction(a):
     return a[1]+a[0]+a[2]
 def main():
     a="a*b"
-    j="hello wrold"
+    j="hello wrold jason"
     ab=prefixfunction(a)
     print(ab)
 if __name__ == '__main__':
